762938e41bcd3263b40a436d00cb5824a0662669
thejennywang <thejennywang@gmail.com>
2015-03-24 17:32:56 -0700

    Cleaned up some logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] rescue nil
   end
 
-  helper_method :current_user, :timeline
+  helper_method :current_user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324195417) do
+ActiveRecord::Schema.define(version: 20150323234714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "home_timelines", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "timelines", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "tweets", force: true do |t|
     t.string   "body"
